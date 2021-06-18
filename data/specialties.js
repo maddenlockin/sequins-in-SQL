6e304ce5f496690672ce8bd0cd86d25f429bfeab
maddenlockin <maddenlockin@gmail.com>
2021-06-18 09:30:03 -0700

    data files updated to specialties

---
+++ data/specialties.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    type: 'cardiology',
+    focus: 'heart',
+    difficulty: 8
+  },
+  {
+    type: 'endocrinology',
+    focus: 'endocrine system',
+    difficulty: 6
+  },
+  {
+    type: 'neurology',
+    focus: 'brain',
+    difficulty: 7
+  },
+  {
+    type: 'psychology',
+    focus: 'mental health',
+    difficulty: 8
+  },
+  {
+    type: 'podiatry',
+    focus: 'feet',
+    difficulty: 5
+  }
+];
